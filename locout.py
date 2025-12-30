979e8ac19478c996acc43b557aaf15dc35d64913
dewiadam <dewi.fitraadam@gmail.com>
2025-12-30 19:20:02 +0900

    Update locout.py/

--- locout.py
+++ locout.py
@@ -14,6 +14,7 @@ import plotly.express as px
 import math
 from scipy.optimize import linear_sum_assignment # Library tambahan utk pemetaan unik
 
+
 # =====================================
 # STYLE UMUM (HEADER, SIDEBAR, BUTTON)
 # =====================================
@@ -763,6 +764,7 @@ elif "Mapping" in menu:
     page_mapping()
 else:
     page_rute()
+
 
 
 
